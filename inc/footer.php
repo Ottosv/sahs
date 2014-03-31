b3cb1262305d8342966d638675301ebcf251084e
Ottosv <jomc-es@hotmail.com>
2014-03-30 20:13:33 -0600

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,4 +1,5 @@
 <?php
 echo '<br />';
 echo '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa provident dolore quidem quo quos voluptatem delectus nam est libero recusandae dolores facere ut quas vero blanditiis similique possimus. Assumenda quas.</p>'
+//Rony una pregunta:  que función tiene este còdigo...?
 ?>
